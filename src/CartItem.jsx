4d8df0f677a766cf0225f58a4f516f1a2cbfc4d4
Yijia Zhang <zhangyijia1997@gmail.com>
2025-03-28 11:23:25 -0400

    Implemented calculateTotalCost()

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -48,6 +48,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
   };
 
   const handleCheckoutShopping = (e) => {
