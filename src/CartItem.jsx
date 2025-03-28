de8e0af23b5a616d684c3202e83af5c58c7fc4b8
Yijia Zhang <zhangyijia1997@gmail.com>
2025-03-28 11:22:01 -0400

    Implemented calculateTotalAmount(), handleIncrement(), handleDecrement(), and handleRemove()

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,49 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => (
+        total += item.quantity * parseFloat(item.cost.substring(1))
+    ));
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    item = {
+        ...item,
+        quantity: item.quantity+1
+    }
+    dispatch(updateQuantity(item));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity == 1) {
+        dispatch(removeItem(item.name));
+        return;
+    }
+    item = {
+        ...item,
+        quantity: item.quantity-1
+    }
+    dispatch(updateQuantity(item));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
